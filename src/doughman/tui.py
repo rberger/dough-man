c4c88e0bc1fe0976fd58cf9a88e01d15b601d9ee
Robert J. Berger <rberger@ibd.com>
2023-11-19 17:02:13 -0800

    Added Config

--- src/doughman/tui.py
+++ src/doughman/tui.py
@@ -1,9 +1,12 @@
 import asyncio
+from textual import on
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Static
+from textual.screen import ModalScreen
+from textual.widgets import Footer, Header, Static, OptionList, TabbedContent, TabPane, Select, Label
 from textual.scroll_view import ScrollView
 from textual.reactive import reactive
 from serial_asyncio import open_serial_connection
+from serial.tools.list_ports import comports
 from vl53_400_lib import AsyncSerialAccess
 
 
@@ -14,10 +17,11 @@ class RiseDisplay(Static):
         super().__init__(*args, **kwargs)
         self.serial_port = serial_port
         self.baud_rate = baud_rate
+        print(f"RiseDisplay: serial_port={self.serial_port}, baud_rate={self.baud_rate}")
         self.rangefinder = AsyncSerialAccess(serial_port=self.serial_port, baud_rate=self.baud_rate)
 
     async def on_mount(self):
-        print("on_mount")
+        print("RiseDisplay: on_mount")
         await self.rangefinder.open_connection()
         asyncio.create_task(self.update_line_output())
 
@@ -57,6 +61,31 @@ class SerialDisplay(Static):
         self.update(line_output)
 
 
+class SerialConfigScreen(Static):
+    """
+    A modal screen that allows user to pick the serial port
+    """
+
+    def __init__(self, serial_ports, baud_rate, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.serial_ports = serial_ports
+        self.baud_rate = baud_rate
+        self.serial_port = None
+
+    def compose(self) -> ComposeResult:
+        yield Label("Serial Port")
+        yield Select(
+            ((port, port) for port in self.serial_ports),
+            id="serial_port",
+            prompt="Select serial port",
+            name="Serial Port",
+        )
+
+    @on(Select.Changed)
+    def select_changed(self, event: Select.Changed) -> None:
+        self.serial_port = str(event.value)
+
+
 class DoughMonApp(App):
     TITLE = "DoughMan TUI"
     BINDINGS = [
@@ -64,15 +93,44 @@ class DoughMonApp(App):
         ("q", "quit", "Quit"),
     ]
 
-    def __init__(self, serial_port, baud_rate, *args, **kwargs):
+    def __init__(self, serial_port=None, baud_rate=115200, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.serial_ports = None
         self.serial_port = serial_port
         self.baud_rate = baud_rate
+        self.set_serial_ports()
+
+    def get_serial_ports(self):
+        ports = comports()
+        for p in ports:
+            print(p.device)
+        serial_ports = [port.device for port in comports() if port.device.startswith("/dev/cu.usbserial")]
+        return serial_ports
 
     def compose(self) -> "ComposeResult":
+        print("APp compose: serial_ports", self.serial_ports)
         yield Header()
-        yield RiseDisplay(serial_port=self.serial_port, baud_rate=self.baud_rate)
+        with TabbedContent(initial="config"):
+            with TabPane("Config", id="config"):
+                yield SerialConfigScreen(serial_ports=self.serial_ports, baud_rate=self.baud_rate)
+            with TabPane("Data", id="data"):
+                yield RiseDisplay(serial_port=self.serial_port, baud_rate=self.baud_rate)
         yield Footer()
+
+    def set_serial_ports(self):
+        serial_ports = self.get_serial_ports()
+        print("serial_ports", serial_ports)
+        if not serial_ports or len(serial_ports) == 0:
+            raise "No serial ports found"
+        if len(serial_ports) == 1:
+            self.serial_ports = serial_ports
+            self.serial_port = serial_ports[0]
+        else:
+            self.serial_ports = serial_ports
+            self.query_one(TabbedContent).active = "config"
+
+    def on_mount(self):
+        self.set_serial_ports()
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
@@ -80,10 +138,10 @@ class DoughMonApp(App):
 
 
 def tui_run() -> None:
-    app = DoughMonApp(serial_port="/dev/tty.usbserial-910", baud_rate=115200)
+    app = DoughMonApp(serial_port=None, baud_rate=115200)
     app.run()
 
 
 if __name__ == "__main__":
-    app = DoughMonApp(serial_port="/dev/tty.usbserial-910", baud_rate=115200)
+    app = DoughMonApp(serial_port=None, baud_rate=115200)
     app.run()
