ecbdca07938db7262979d329c2fe1f0aa95029fc
Robert J. Berger <rberger@ibd.com>
2023-11-20 00:55:27 -0800

    Got baud_rate and serial port update working

--- src/doughman/tui.py
+++ src/doughman/tui.py
@@ -1,6 +1,7 @@
 import asyncio
 from textual import on
 from textual.app import App, ComposeResult
+from textual.message import Message
 from textual.screen import ModalScreen
 from textual.widgets import Footer, Header, Static, OptionList, TabbedContent, TabPane, Select, Label
 from textual.scroll_view import ScrollView
@@ -10,35 +11,16 @@ from serial.tools.list_ports import comports
 from vl53_400_lib import AsyncSerialAccess
 
 
-class RiseDisplay(Static):
-    line_output = reactive("")
-
-    def __init__(self, serial_port, baud_rate, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.serial_port = serial_port
-        self.baud_rate = baud_rate
-        print(f"RiseDisplay: serial_port={self.serial_port}, baud_rate={self.baud_rate}")
-        self.rangefinder = AsyncSerialAccess(serial_port=self.serial_port, baud_rate=self.baud_rate)
-
-    async def on_mount(self):
-        print("RiseDisplay: on_mount")
-        await self.rangefinder.open_connection()
-        asyncio.create_task(self.update_line_output())
-
-    async def update_line_output(self):
-        self.update("Waiting for data...")
-        while True:
-            result = await self.rangefinder.get_distance()
-            self.line_output = f"Distance: {result['distance']} {result['units']}"
-
-    def watch_line_output(self, line_output: str) -> None:
-        self.update(line_output)
-
-
-class SerialConfigScreen(Static):
+class SerialConfig(Static):
     """
     A modal screen that allows user to pick the serial port
     """
+
+    class Changed(Message):
+        def __init__(self, select: str, value: str):
+            self.select = select
+            self.value = value
+            super().__init__()
 
     def __init__(self, serial_ports, baud_rate, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -54,10 +36,49 @@ class SerialConfigScreen(Static):
             prompt="Select serial port",
             name="Serial Port",
         )
+        yield Label("Baud Rate")
+        yield Select(
+            (
+                ("9600", 9600),
+                ("115200", 115200),
+            ),
+            id="baud_rate",
+            prompt="Select baud rate",
+            name="Baud Rate",
+        )
 
     @on(Select.Changed)
     def select_changed(self, event: Select.Changed) -> None:
-        self.serial_port = str(event.value)
+        match event.select:
+            case "baud_rate":
+                self.baud_rate = event.value
+            case "serial_port":
+                self.serial_port = str(event.value)
+        self.post_message(self.Changed(select=event.select, value=event.value))
+
+
+class RiseDisplay(Static):
+    line_output = reactive("")
+
+    def __init__(self, serial_port, baud_rate, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.serial_port = serial_port
+        self.baud_rate = baud_rate
+        print(f"RiseDisplay: serial_port={self.serial_port}, baud_rate={self.baud_rate}")
+        self.rangefinder = AsyncSerialAccess(serial_port=self.serial_port, baud_rate=self.baud_rate)
+
+    async def on_mount(self):
+        await self.rangefinder.open_connection()
+        asyncio.create_task(self.update_line_output())
+
+    async def update_line_output(self):
+        self.update("Waiting for data...")
+        while True:
+            result = await self.rangefinder.get_distance()
+            self.line_output = f"Distance: {result['distance']} {result['units']}"
+
+    def watch_line_output(self, line_output: str) -> None:
+        self.update(line_output)
 
 
 class DoughMonApp(App):
@@ -76,24 +97,20 @@ class DoughMonApp(App):
 
     def get_serial_ports(self):
         ports = comports()
-        for p in ports:
-            print(p.device)
         serial_ports = [port.device for port in comports() if port.device.startswith("/dev/cu.usbserial")]
         return serial_ports
 
     def compose(self) -> "ComposeResult":
-        print("APp compose: serial_ports", self.serial_ports)
         yield Header()
-        with TabbedContent(initial="config"):
+        with TabbedContent(initial="data"):
             with TabPane("Config", id="config"):
-                yield SerialConfigScreen(serial_ports=self.serial_ports, baud_rate=self.baud_rate)
+                yield SerialConfig(serial_ports=self.serial_ports, baud_rate=self.baud_rate)
             with TabPane("Data", id="data"):
-                yield RiseDisplay(serial_port=self.serial_port, baud_rate=self.baud_rate)
+                yield RiseDisplay(serial_port=self.serial_port, baud_rate=self.baud_rate, id="rise_display")
         yield Footer()
 
     def set_serial_ports(self):
         serial_ports = self.get_serial_ports()
-        print("serial_ports", serial_ports)
         if not serial_ports or len(serial_ports) == 0:
             raise "No serial ports found"
         if len(serial_ports) == 1:
@@ -110,12 +127,37 @@ class DoughMonApp(App):
         """An action to toggle dark mode."""
         self.dark = not self.dark
 
+    def on_serial_config_changed(self, message: SerialConfig.Changed) -> None:
+        if message.select.id == "serial_port":
+            self.serial_port = message.value
+        elif message.select.id == "baud_rate":
+            self.baud_rate = message.value
+        self.exit({"restart": True, "serial_port": self.serial_port, "baud_rate": self.baud_rate})
+
 
 def tui_run() -> None:
-    app = DoughMonApp(serial_port=None, baud_rate=115200)
-    app.run()
+    serial_port = None
+    baud_rate = 115200
+    restart = True
+    while restart:
+        app = DoughMonApp(serial_port=serial_port, baud_rate=baud_rate)
+        result = app.run()
+        if result and result["restart"]:
+            serial_port = result["serial_port"]
+            baud_rate = result["baud_rate"]
+        else:
+            restart = False
 
 
 if __name__ == "__main__":
-    app = DoughMonApp(serial_port=None, baud_rate=115200)
-    app.run()
+    serial_port = None
+    baud_rate = 115200
+    restart = True
+    while restart:
+        app = DoughMonApp(serial_port=serial_port, baud_rate=baud_rate)
+        result = app.run()
+        if result and result["restart"]:
+            serial_port = result["serial_port"]
+            baud_rate = result["baud_rate"]
+        else:
+            restart = False
